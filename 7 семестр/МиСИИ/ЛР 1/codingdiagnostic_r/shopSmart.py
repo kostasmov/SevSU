20511c6c2da2bd65dee7ff730a5ac8dfc92b6f54
Konstantin Movenko <kostasmov@mail.ru>
2025-11-10 12:50:31 +0300

    it gonna be tough

--- 7 семестр/МиСИИ/ЛР 1/codingdiagnostic_r/shopSmart.py
+++ 7 семестр/МиСИИ/ЛР 1/codingdiagnostic_r/shopSmart.py
@@ -46,12 +46,16 @@ def shopSmart(orderList, fruitShops):
 
 if __name__ == '__main__':
     "Этот код выполняется, когда Вы запускаете скрипт из командной строки"
-    orders = [('apples', 1.0), ('oranges', 3.0)]
     dir1 = {'apples': 2.0, 'oranges': 1.0}
     shop1 = shop.FruitShop('shop1', dir1)
+
     dir2 = {'apples': 1.0, 'oranges': 5.0}
     shop2 = shop.FruitShop('shop2', dir2)
+
     shops = [shop1, shop2]
+
+    orders = [('apples', 1.0), ('oranges', 3.0)]
     print("For orders ", orders, ", the best shop is", shopSmart(orders, shops).getName())
+
     orders = [('apples', 3.0)]
     print("For orders: ", orders, ", the best shop is", shopSmart(orders, shops).getName())
