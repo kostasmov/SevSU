0959862fd8c03a3d0208825f9e4d26bef09a532d
Konstantin Movenko <kostasmov@mail.ru>
2026-06-12 13:53:28 +0300

    wtf aaa ohh

--- 8 семестр/ВКР/cplex_scheduler/model/__init__.py
+++
@@ -1,3 +0,0 @@
-from model.parameters import TaskParameters
-from model.results import OptimizationResults, BatchInfo, ScheduleEntry, MaintenanceEntry
-from model.milp_model import MILPModel, SOLVER_AVAILABLE
